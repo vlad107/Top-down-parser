98a078b391094d5e554c3789fa5ce8ec31ca7bef
vlad107 <vlad10795@gmail.com>
2017-02-16 00:44:45 +0300

    add visualizer

--- app/Main.hs
+++ app/Main.hs
@@ -1,13 +1,19 @@
 module Main where
 
 import Lib
+import Visualizer
 import System.Environment (getArgs)
 import Lexer
+import Data.Text.Lazy (pack)
 import Parser
+import Data.Bifunctor
+import Control.Monad (join)
 
 main :: IO ()
 main = do 
 	args <- getArgs
 	content <- readFile (args !! 0)
-	print (parseExpr (tokenizeAll (words content)))
+	case (join (toGraph <$> fst <$> (parseExpr (tokenizeAll (words content))))) of 
+		Just x -> visualize x
+		Nothing -> print "Did not match"
 
--- src/Parser.hs
+++ src/Parser.hs
@@ -2,10 +2,39 @@ module Parser where
 
 import Lexer
 import Data.Bifunctor
+import Data.Text.Lazy (Text, pack)
+
+class ToGraph a where 
+	toGraphDef :: a -> Int -> Maybe (Int, [(Int, Text)], [(Int, Int)])
+	toGraph :: a -> Maybe ([(Int, Text)], [(Int, Int, Text)])
+	toGraph e = do 
+				(cur, vs, es) <- toGraphDef e 0
+				return (vs, [(a, b, pack "") | (a, b) <- es])
 
 data Expr = Expr Int ERem         deriving Show
 data ERem = ERem E' | Eps         deriving Show
 data E'   = E' Int ERem Char ERem deriving Show
+
+instance ToGraph Expr where 
+	toGraphDef (Expr n erem) cur = do
+									(cur', vs, es) <- toGraphDef erem cur
+									return (cur' + 1, [(cur', pack "Expr")] ++ vs, [(cur', cur' - 1)] ++ es)
+
+instance ToGraph ERem where 
+	toGraphDef (ERem e') cur = do
+								(cur', vs, es) <- toGraphDef e' cur
+								return (cur' + 1, [(cur', pack "ERem")] ++ vs, [(cur', cur' - 1)] ++ es)
+	toGraphDef _ cur = Just (cur + 1, [(cur, pack "Eps")], [])
+								
+instance ToGraph E' where
+	toGraphDef (E' n erem1 o erem2) cur = do
+											(cur', vs, es)    <- toGraphDef erem1 cur
+											(cur'', vs', es') <- toGraphDef erem2 cur'
+											return (cur'' + 3, 
+													[(cur'' + 2, pack "E'")] ++ [(cur'' + 1, pack $ show n)] ++ [(cur'', pack $ show o)] ++ vs ++ vs', 
+													[(cur'' + 2, cur'' + 1)] ++ [(cur'' + 2, cur'' - 1)] ++ [(cur'' + 2, cur'')] ++ [(cur'' + 2, cur' - 1)] ++ es ++ es')
+
+
 
 parseExpr :: [Token] -> Maybe (Expr, [Token])
 parseExpr (Number n : rem) = first (Expr n) <$> parseERem rem 
